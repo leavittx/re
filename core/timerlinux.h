b519a085dfc845d6fa4bcdf50ad371b2cb05f2e9
Lev <lev.panov@gmail.com>
2011-09-25 17:33:34 +0400

    temporary workaround to avoid strange timer behavior on linux

--- core/timerlinux.h
+++ core/timerlinux.h
@@ -1,7 +1,9 @@
 #pragma once
 
-#include <ctime>
+#include <sys/time.h>
 #include "timer.h"
+
+#include "util/debug.h"
 
 namespace recore {
 
@@ -14,9 +16,15 @@ public:
 private:
 	virtual TimeT osGetTimeMsec()
 	{
-		static struct timespec time;
-		clock_gettime(CLOCK_REALTIME, &time);
-		return (int)(time.tv_nsec / 1000000.0);
+		struct timespec ts;
+		clock_gettime(CLOCK_MONOTONIC, &ts);
+
+		TimeT msec = (TimeT)(ts.tv_nsec / 1000000.0);
+
+		reutil::g_debug << "time: " << ts.tv_nsec << " (in milliseconds: " << msec << ")" << std::endl;
+
+//		return msec;
+		return clock() / 1000.0;
 	}
 };
 
