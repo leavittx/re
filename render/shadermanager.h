8385b7f1dd526a3db77cd8a3885d0c4555b6fbae
Lev <lev.panov@gmail.com>
2011-10-07 12:25:07 +0400

    shadermanager: non-functional changes

--- render/shadermanager.h
+++ render/shadermanager.h
@@ -11,43 +11,49 @@ class Shader;
 #ifdef __GLTOOLS_MY__
 
 enum GLT_STOCK_SHADER { GLT_SHADER_IDENTITY = 0, GLT_SHADER_FLAT, GLT_SHADER_SHADED, GLT_SHADER_DEFAULT_LIGHT, GLT_SHADER_POINT_LIGHT_DIFF,
-	GLT_SHADER_TEXTURE_REPLACE, GLT_SHADER_TEXTURE_MODULATE, GLT_SHADER_TEXTURE_POINT_LIGHT_DIFF, GLT_SHADER_TEXTURE_RECT_REPLACE,
-	GLT_SHADER_LAST };
+						GLT_SHADER_TEXTURE_REPLACE, GLT_SHADER_TEXTURE_MODULATE, GLT_SHADER_TEXTURE_POINT_LIGHT_DIFF, GLT_SHADER_TEXTURE_RECT_REPLACE,
+						GLT_SHADER_LAST };
 
 enum GLT_SHADER_ATTRIBUTE { GLT_ATTRIBUTE_VERTEX = 0, GLT_ATTRIBUTE_COLOR, GLT_ATTRIBUTE_NORMAL,
-	GLT_ATTRIBUTE_TEXTURE0, GLT_ATTRIBUTE_TEXTURE1, GLT_ATTRIBUTE_TEXTURE2, GLT_ATTRIBUTE_TEXTURE3,
-	GLT_ATTRIBUTE_LAST};
+							GLT_ATTRIBUTE_TEXTURE0, GLT_ATTRIBUTE_TEXTURE1, GLT_ATTRIBUTE_TEXTURE2, GLT_ATTRIBUTE_TEXTURE3,
+							GLT_ATTRIBUTE_LAST};
 
 #endif // __GLTOOLS_MY__
 
 struct StockShaderUniforms
 {
 	// Everything
-	StockShaderUniforms(float* _mvpMatrix, float* _mvMatrix, float* _pMatrix, float* _vLightPos, float* _vColor, int _textureUnit0 = 0) :
-		mvpMatrix(_mvpMatrix), mvMatrix(_mvMatrix), pMatrix(_pMatrix), vLightPos(_vLightPos), vColor(_vColor), textureUnit0(_textureUnit0) {}
+	StockShaderUniforms(float* _mvpMatrix, float* _mvMatrix, float* _pMatrix, float* _vLightPos, float* _vColor, int _textureUnit0 = 0)
+		: mvpMatrix(_mvpMatrix), mvMatrix(_mvMatrix), pMatrix(_pMatrix), vLightPos(_vLightPos), vColor(_vColor), textureUnit0(_textureUnit0) {}
 	// Just the modelview projection matrix and the color (FLAT shader)
-	StockShaderUniforms(float* _mvpMatrix, float* _vColor) : mvpMatrix(_mvpMatrix), vColor(_vColor) {}
+	StockShaderUniforms(float* _mvpMatrix, float* _vColor)
+		: mvpMatrix(_mvpMatrix), vColor(_vColor) {}
 	// Just the texture place (TEXTURE_REPLACE shader)
-	StockShaderUniforms(float* _mvpMatrix, int _textureUnit0) : mvpMatrix(_mvpMatrix), textureUnit0(_textureUnit0) {}
+	StockShaderUniforms(float* _mvpMatrix, int _textureUnit0)
+		: mvpMatrix(_mvpMatrix), textureUnit0(_textureUnit0) {}
 	// TEXTURE_MODULATE shader
-	StockShaderUniforms(float* _mvpMatrix, float* _vColor, int _textureUnit0) : mvpMatrix(_mvpMatrix), vColor(_vColor), textureUnit0(_textureUnit0) {}
+	StockShaderUniforms(float* _mvpMatrix, float* _vColor, int _textureUnit0)
+		: mvpMatrix(_mvpMatrix), vColor(_vColor), textureUnit0(_textureUnit0) {}
 	// DEFAULT_LIGHT shader
-	StockShaderUniforms(float* _mvMatrix, float* _pMatrix, float* _vColor) : mvMatrix(_mvMatrix), pMatrix(_pMatrix), vColor(_vColor) {}
+	StockShaderUniforms(float* _mvMatrix, float* _pMatrix, float* _vColor)
+		: mvMatrix(_mvMatrix), pMatrix(_pMatrix), vColor(_vColor) {}
 	// POINT_LIGHT_DIFF shader
-	StockShaderUniforms(float* _mvMatrix, float* _pMatrix, float* _vLightPos, float* _vColor) : mvMatrix(_mvMatrix), pMatrix(_pMatrix), vLightPos(_vLightPos), vColor(_vColor) {}
+	StockShaderUniforms(float* _mvMatrix, float* _pMatrix, float* _vLightPos, float* _vColor)
+		: mvMatrix(_mvMatrix), pMatrix(_pMatrix), vLightPos(_vLightPos), vColor(_vColor) {}
 	// TEXTURE_POINT_LIGHT_DIFF shader
 	StockShaderUniforms(float* _mvMatrix, float* _pMatrix, float* _vLightPos, float* _vColor, int _textureUnit0) :
 		mvMatrix(_mvMatrix), pMatrix(_pMatrix), vLightPos(_vLightPos), vColor(_vColor), textureUnit0(_textureUnit0) {}
 	// Just the modelview projection matrix. Color is an attribute (SHADED shader :))
-	StockShaderUniforms(float* _mvpMatrix) : mvpMatrix(_mvpMatrix) {}
+	StockShaderUniforms(float* _mvpMatrix)
+		: mvpMatrix(_mvpMatrix) {}
 	// Just the Color (IDENTITY shader)
-//	StockShaderUniforms(float* _vColor) : vColor(_vColor) {}
+	//	StockShaderUniforms(float* _vColor) : vColor(_vColor) {}
 
-//	M3DMatrix44f* mvpMatrix;
-//	M3DMatrix44f* mvMatrix;
-//	M3DMatrix44f* pMatrix;
-//	M3DVector3f* vLightPos;
-//	M3DVector4f* vColor;
+	//	M3DMatrix44f* mvpMatrix;
+	//	M3DMatrix44f* mvMatrix;
+	//	M3DMatrix44f* pMatrix;
+	//	M3DVector3f* vLightPos;
+	//	M3DVector4f* vColor;
 	float* mvpMatrix;
 	float* mvMatrix;
 	float* pMatrix;
