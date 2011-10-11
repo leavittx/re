a808ec59aafdf862737d9ac8261d731bab06490b
Lev Panov <lev.panov@gmail.com>
2011-10-11 23:06:59 +0400

    globals.h clean-up

--- globals.h
+++ globals.h
@@ -1,29 +1,21 @@
 #pragma once
 
-//#include <GL/glew.h>
-//#include <GL/gl.h>
-//#include <GL/glu.h>
-
 #include <string>
 #include <vector>
 #include <map>
 #include <sstream>
 #include <cstdlib>
-//#include <cmath>
 
 typedef unsigned int TimeT;
 
 #define __SDL__ 0
 #define __X11__ 1
 
-#define __GLTOOLS_MY__
-
 #include "util/debug.h"
 #include "util/singleton.h"
 
 #include "core/keyboard.h"
 #include "core/inputmanager.h"
-//#include "core/time.h"
 
 #include "demo/scene.h"
 
@@ -31,6 +23,3 @@ typedef unsigned int TimeT;
 
 #include "render/glhelper.h"
 #include "render/shadermanager.h"
-//#include "render/texturemanager.h"
-
-//#include "util/templateinstantiations.cpp"
